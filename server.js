8e43083ca692a25496fa6c1245c85b4d98d3846a
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2020-01-02 14:15:18 -0500

    ver 1.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const postsController = require('./controllers/Posts.js');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -18,9 +19,6 @@ app.use(bodyParser.json());
 // DB config
 const db = process.env.MONGODB_URI;
 
-// Port config
-const port = process.env.PORT;
-
 // Connect to Mongo
 mongoose
     .connect(db, { useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex: true })
@@ -30,14 +28,17 @@ mongoose
 // Controllers
 app.use('/posts', postsController)
 
-// // Serve static assets if in production
-// if(process.env.NODE_ENV === 'production') {
-//     // set static folder
-//     app.use(express.static('my-dev-corner/build'));
-//
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'my-dev-corner', 'build', 'index.html'));
-//     })
-// }
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('my-dev-corner/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'my-dev-corner', 'build', 'index.html'));
+    });
+}
+
+// Port config
+const port = process.env.PORT;
 
     app.listen(port, () => console.log(`Server started on port ${port}`));
