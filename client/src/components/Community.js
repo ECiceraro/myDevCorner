4cdd4169c033fccda4ebf6d5dd337116f54c25a0
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2020-01-02 16:40:25 -0500

    cont debugging heroku/mongo issues 5

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -11,7 +11,7 @@ class Community extends React.Component {
     }
 
     componentDidMount(){
-        const queryUrl = `https://savvyhotspotsapi.herokuapp.com/api/controllers/posts`;
+        const queryUrl = `https://savvyhotspotsapi.herokuapp.com/posts`;
         axios(queryUrl)
         .then(response => {
             console.log(response);
