7a5d119eda132d143d2aa1abca9b49a836ed6a33
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2020-01-04 00:10:48 -0500

    finished front-end and back-end overview pages, all mobile responsive

--- client/src/components/Backend.js
+++ client/src/components/Backend.js
@@ -4,9 +4,19 @@ import './Backend.css';
 class Backend extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Back End Overview</h1>
-            </div>
+            <>
+                <div className="heroDiv2">
+                    <img className="heroImg2" src="/images/frontendHero.png" alt="laptop signifying back-end development" />
+                    <h1 className="heroTitle2">Back-End</h1>
+                </div>
+                <h3 className="subTitle1Fe">What is back-end development?</h3>
+                <div className="subDiv1Fe">
+                    <div className="subtextBox1Fe">
+                        <p>Back end development refers to the server side of an application and everything that communicates between the database and the browser like APIs. Back end Development refers to the server side of development where you are primarily focused on how the site works.</p>
+                    </div>
+                        <iframe className="frontendVid" title="Back-End Dev Explained" width="350" height="315" src="https://www.youtube.com/embed/WwbBOQaM0Zw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+            </>
         )
     }
 }
--- client/src/components/Frontend.js
+++ client/src/components/Frontend.js
@@ -4,9 +4,21 @@ import './Frontend.css';
 class Frontend extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Frontend Overview</h1>
-            </div>
+            <>
+                <div className="heroDiv2">
+                    <img className="heroImg2" src="/images/frontendHero.png" alt="laptop signifying front end development" />
+                    <h1 className="heroTitle2">Front-End</h1>
+                </div>
+                <h3 className="subTitle1Fe">What is front-end development?</h3>
+                <div className="subDiv1Fe">
+                    <div className="subtextBox1Fe">
+                        <p>A front-end developer links together the world of design and technology, packaging up the utility of the back end in an inviting way for users to interact with. They take website design files and convert them into HTML, JavaScript (JS) and/or CSS code - the core elements of front-end development.</p>
+                    </div>
+
+                        <iframe className="frontendVid" title="Front End Dev Explained" width="350" height="315" src="https://www.youtube.com/embed/GJ8jidDdWVg" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+                </div>
+            </>
         )
     }
 }
