a02e6b140cb8bf14d462157c0eb7cb115f139651
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2020-01-06 10:43:57 -0500

    trying to create users now using live link from heroku

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import './Signup.css';
+import axios from 'axios';
+const baseUrl = `https://mydevcorner.herokuapp.com`;
 
 class Signup extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    // ==============
+    // HANDLERS
+    // ==============
+    // handles form change
+    handleChange = (e) => {
+      this.setState({[e.target.id] : e.target.value})
+    }
+    // handles creating user
+    handleCreate = (createData) => {
+        axios(`${baseUrl}/users`, {
+            body: JSON.stringify(createData),
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(createdUser => {
+            return createdUser.json()
+        }).catch(err => console.log(err))
+    }
+    // handles submit
+    handleSubmit = (e) => {
+      e.preventDefault();
+      this.handleCreate(this.state);
+    }
     render() {
         return (
             <>
@@ -9,15 +44,15 @@ class Signup extends React.Component {
                     <img className="heroImg2" src="/images/frontendHero.png" alt="Create Account icon" />
                     <h1 className="heroTitle2">Create Account</h1>
                 </div>
-                <form className="signupForm">
+                <form onSubmit={this.handleSubmit} className="signupForm">
                     <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Username</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username"/>
+                    <input type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter Username" value={this.state.username} onChange={this.handleChange}/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handleChange}/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
