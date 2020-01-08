0df442673647a4ca53e6a4e18dae06eba71f633d
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2020-01-08 06:03:04 -0500

    finished last edits

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,15 +27,15 @@ class Home extends React.Component {
             <div className="subtextBoxContainer ">
                 <div className="subtextBox1">
                     <h2 className="subtextTitle1">Tech <br /> Overviews</h2>
-                    <p className="subtext">Check out the tech overviews in the navbar at the top of the page to get an index of snap shots of each technology I've worked with.</p>
+                    <p className="subtext">Check out the technologies I've worked with by navigating through the tabs on the navbar. Click on the icon for each technologyto uncover useful links.</p>
                 </div>
                 <div className="subtextBox2">
                     <h2 className="subtextTitle2">Create <br /> An Account</h2>
-                    <p className="subtext">Check out the tech overviews in the navbar at the top of the page to get an index of snap shots of each technology I've worked with.</p>
+                    <p className="subtext">Creating an account at myDevCorner couldn't be more easy, simply click the "Sign Up" button in the nav and create a user account... and you're in! </p>
                 </div>
                 <div className="subtextBox3">
                     <h2 className="subtextTitle3">Community</h2>
-                    <p className="subtext3">After you've made an account, login and create a post on our dev forum. It can be a question, thought, code, whatever dev related thing you'd like.</p>
+                    <p className="subtext3">Once you've made an account, login to create a post or leave a comment on our dev forum. It can be a question, thought, code, whatever dev related thing you'd like.</p>
                 </div>
             </div>
             </>
